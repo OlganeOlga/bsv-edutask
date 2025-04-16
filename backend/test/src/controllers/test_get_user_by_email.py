affa0ebb38051a76f9f560690dbcb85d7cf1f3d5
Ольга Егорова <107860466+OlganeOlga@users.noreply.github.com>
2025-04-16 23:25:20 +0200

    Update test_get_user_by_email.py

--- backend/test/src/controllers/test_get_user_by_email.py
+++ backend/test/src/controllers/test_get_user_by_email.py
@@ -39,9 +39,26 @@ def test_valid_email_no_users(user_controller, invalid_email):
 
 
 def test_valid_email_no_users(user_controller, mock_dao):
+  # Create value that returns mock_dao
   mock_dao().find.return_value = []
-
+  # Assert result
   with patch('re.fullmatch', return_value=True):
       result = self.controller.get_user_by_email("dao.duner@domain.host")
       assert result == None
-      
+
+def test_valid_email_one_user():
+  # Create value that returns mock_dao
+  self.mock_dao().find.return_value = [self.user]
+  # Assert result
+  with patch('re.fullmatch', return_value=True):
+    result = self.user_controller.get_user_by_email(self.user.email)
+    assert result.isObjeckt()
+    assert result == self.user
+
+def test_valid_email_several_user():
+  # Create value that returns mock_dao
+  self.mock_dao().find.return_value = [self.users]
+  # Assert result
+  with patch('re.fullmatch', return_value=True):
+      result = self.user_controller.get_user_by_email(self.user.email)
+      assert result == self.user
