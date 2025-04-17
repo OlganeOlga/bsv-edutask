d2a701e592858852b1dfd966db46bea7ccef565f
OlganeOlga <o_yegorova@yahoo.com>
2025-04-17 15:17:54 +0200

    ajusting

--- backend/test/src/controllers/test_get_user_by_email.py
+++ backend/test/src/controllers/test_get_user_by_email.py
@@ -53,12 +53,13 @@ def valid_email():
     return "dao.duner@domain.host"
 
 # TEST CASES for get_user_by_email
+# C1
 def test_invalid_email(user_controller, invalid_email):
   # test if ValueError rises with invalid email
    with patch('re.fullmatch', return_value=False):
     with pytest.raises(ValueError):
         user_controller.get_user_by_email(invalid_email)
-
+# C2
 def test_valid_email_no_users(user_controller, mock_dao, valid_email):
   # test if None return with no users having valid email
   # Create value that returns mock_dao
@@ -67,7 +68,7 @@ def test_valid_email_no_users(user_controller, mock_dao, valid_email):
     result = user_controller.get_user_by_email(valid_email)
     assert result == None
 
-
+# C3
 def test_valid_email_one_user(user_controller, mock_dao, valid_email, one_user):
   # Create value that returns mock_dao
   mock_dao.find.return_value = one_user
@@ -77,6 +78,7 @@ def test_valid_email_one_user(user_controller, mock_dao, valid_email, one_user):
     print(result)
     assert result == one_user[0]
 
+# C4-1
 def test_valid_email_several_user(user_controller, mock_dao, valid_email, two_users):
   # Create value that returns mock_dao
   mock_dao.find.return_value = two_users
@@ -86,6 +88,21 @@ def test_valid_email_several_user(user_controller, mock_dao, valid_email, two_us
       print(result)
       assert result == two_users[0]
 
+# C4-2
+def test_valid_email_several_user_prints_warning(user_controller, mock_dao, valid_email, two_users):
+    # Simulate the DAO returning more than one user
+    mock_dao.find.return_value = two_users
+    
+    # assert if output match expected
+    with patch('re.fullmatch', return_value=True):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output  # redirect stdout
+        result = user_controller.get_user_by_email(valid_email)
+        sys.stdout = sys.__stdout__  # reset redirect
+        assert "more than one user found with mail" in captured_output.getvalue()
+        assert valid_email in captured_output.getvalue()
+  
+# C5      
 def test_database_failure_raises_exception(user_controller, mock_dao,valid_email):
   # Simulate the DAO throwing an exception
   mock_dao.find.side_effect = Exception("Database failure")
